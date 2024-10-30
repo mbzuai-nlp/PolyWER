5b55a65e322177075aeebd6e6a2a01f482470a11
Karima Kadaoui <kariima.kad@gmail.com>
2024-10-30 19:59:54 +0400

    Add readme and toy_example to include other metrics, add mrwer format refs and hyps

--- README.md
+++ README.md
@@ -41,6 +41,38 @@ pip install -r requirements.txt
 python toy_example.py
 ```
 
+The toy example includes two sentences with their 3 transcription dimensions and outputs the different metrics we've used in our evaluation (PolyWER, WER, CER, BLEU, BERTScore).
+To run [multiRefWer](https://github.com/qcri/multiRefWER), please do the following:
+
+```bash
+git clone https://github.com/qcri/multiRefWER 
+cd multiRefWER
+mrwer.py -e <polywer_path>/ref_og <polywer_path>/ref_lit <polywer_path>/ref_lat <polywer_path>/hyp 
+```
+
+Please note that we had to modify the mrwer code slightly to be able to run it (adding parentheses to the print statements and commenting out sys.reload)
+
+
+## Dataset
+The Mixat dataset with the additional transcriptions can be found on [HuggingFace](https://huggingface.co/datasets/sqrk/mixat-tri).
+
+```python
+>>> from datasets import load_dataset
+>>> mixat = load_dataset("sqrk/mixat-tri")
+>>> mixat
+DatasetDict({
+    train: Dataset({
+        features: ['audio', 'transcript', 'transliteration', 'translation', 'language', 'duration_ms'],
+        num_rows: 3727
+    })
+    test: Dataset({
+        features: ['audio', 'transcript', 'transliteration', 'translation', 'language', 'duration_ms'],
+        num_rows: 1585
+    })
+})
+
+```
+
 
 # Acknowledgements
 If you use any PolyWER, please cite 
--- toy_example.py
+++ toy_example.py
@@ -1,20 +1,10 @@
-from polywer_algorithm import polywer, polywer_multi
+from polywer_algorithm import polywer_multi, cleanList
+from jiwer import wer, cer
+import evaluate 
 
 cer_threshold = 0.25
 cosine_threshold = 0.85
 
-# Individual (ref,hyp) pair
-r = [
-        '[Biochemistry, pharmacology] وهني [physics] و [Materials] والأمور هاي.', 
-        '[بايوكيميستري، فارماكولوجي] وهني [فيزيكس] و [ماتيريالز] والأمور هاي.',
-        '[الكيمياء الحيوية، الصيدلة] وهني [الفيزياء] و [المواد] والأمور هاي.'
-]
-
-h = 'biochemistry فارماكولوجي وهنى الفيزياء و materials والأمور هاي'
-
-print(polywer(r, h, cer_threshold, cosine_threshold))
-
-# Multiple (ref,hyp) pairs
 
 rs = []
 rs.append([
@@ -34,4 +24,34 @@ hs = [
     'My passion واز أركيتكتشور من البداية أنا كنت أعرف هالشي'
 ]
 
-print(polywer_multi(rs, hs, cer_threshold, cosine_threshold))
+############################ POLYWER ############################
+
+poly_result = polywer_multi(rs, hs, cer_threshold, cosine_threshold)
+
+############################ WER, CER ############################
+rs = [cleanList(item) for item in rs]
+hs = [cleanList(item) for item in hs]
+
+wer_result = wer(rs, hs)
+cer_result = cer(rs, hs)
+
+############################ BLEU ############################
+
+bleu = evaluate.load('bleu')
+bleu_result = bleu.compute(references=rs, predictions=hs)['bleu']
+
+############################ BERTScore ############################
+
+bertscore = evaluate.load('bertscore')
+bertscore_result = bertscore.compute(references=rs, predictions=hs, model_type="google/mt5-large")['f1'][0]
+
+############################ mrWER ############################
+
+'''
+        Clone the repo https://github.com/qcri/multiRefWER and run:
+        <mrwer_path>/mrwer.py -e <polywer_path>/ref_og <polywer_path>/ref_lit <polywer_path>/ref_lat <polywer_path>/hyp 
+
+        Please note that we had to modify the mrwer code slightly to be able to run it (adding parentheses to the print statements and commenting out sys.reload)
+'''
+
+print(f'PolyWER: {poly_result}\nWER: {wer_result}\nCER: {cer_result}\nBLEU: {bleu_result}\nBertScore: {bertscore_result}')
